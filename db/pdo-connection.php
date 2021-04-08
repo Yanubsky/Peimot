0d84afca9141b97eab1768874dacb2d1eec67b20
yaniv Moalem <nivniv1982@gmail.com>
2021-04-08 16:08:14 +0300

    cleardb conn attemption and title name in home.php

--- db/pdo-connection.php
+++ db/pdo-connection.php
@@ -6,7 +6,22 @@ try {
     $dbpass = 'Clinton55'; 
     $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     echo "Error : " . $e->getMessage() . "<br/>";
     die();
 }
+
+?>
+
+<?php
+//Get Heroku ClearDB connection information
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
+
+// $con = new mysqli($server, $username, $password, $db);
+?>
